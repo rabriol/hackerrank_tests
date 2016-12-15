2c5b4c564d8e0ca350947f5c770844ba2c364987
Rafael Brito de Oliveira <peo_rboliveira@uolinc.com>
2016-12-15 14:29:58 -0200

    adding all the others algorithms tests

--- src/BinarySearchTree.java
+++ src/BinarySearchTree.java
@@ -5,50 +5,50 @@ public class BinarySearchTree {
 
         public static void main(String[] args) {
 
-            Node one = new Node(1, null, null);
+            Node1 one = new Node1(1, null, null);
 
-            Node four = new Node(4, null, null);
-            Node seven = new Node(7, null, null);
-            Node six = new Node(6, four, seven);
+            Node1 four = new Node1(4, null, null);
+            Node1 seven = new Node1(7, null, null);
+            Node1 six = new Node1(6, four, seven);
 
-            Node three = new Node(3, one, six);
+            Node1 three = new Node1(3, one, six);
 
-            Node thirteen = new Node(13, null, null);
-            Node fourteen = new Node(14, thirteen, null);
-            Node ten = new Node(10, null, fourteen);
-            Node eigth = new Node(8, three, ten);
+            Node1 thirteen = new Node1(13, null, null);
+            Node1 fourteen = new Node1(14, thirteen, null);
+            Node1 ten = new Node1(10, null, fourteen);
+            Node1 eigth = new Node1(8, three, ten);
 
             System.out.println(checkBTS(eigth));
         }
 
-        public static boolean checkBTS(Node node) {
-            return checkSubBTS(Integer.MIN_VALUE, Integer.MAX_VALUE, node);
+        public static boolean checkBTS(Node1 Node1) {
+            return checkSubBTS(Integer.MIN_VALUE, Integer.MAX_VALUE, Node1);
         }
 
-        public static boolean checkSubBTS(int min, int max, Node node) {
-            if (node == null) return true;
-            if (max < node.getValue() || min > node.getValue()) return false;
-            return checkSubBTS(min, node.getValue(), node.getLeft()) && checkSubBTS(node.getValue(), max, node.getRight());
+        public static boolean checkSubBTS(int min, int max, Node1 Node1) {
+            if (Node1 == null) return true;
+            if (max < Node1.getValue() || min > Node1.getValue()) return false;
+            return checkSubBTS(min, Node1.getValue(), Node1.getLeft()) && checkSubBTS(Node1.getValue(), max, Node1.getRight());
         }
 
     }
 
-    class Node {
-        private Node left;
-        private Node right;
+    class Node1 {
+        private Node1 left;
+        private Node1 right;
         private int value;
 
-        public Node(int value, Node left, Node right) {
+        public Node1(int value, Node1 left, Node1 right) {
             this.left = left;
             this.right = right;
             this.value = value;
         }
 
-        public Node getLeft() {
+        public Node1 getLeft() {
             return left;
         }
 
-        public Node getRight() {
+        public Node1 getRight() {
             return right;
         }
 
@@ -58,7 +58,7 @@ public class BinarySearchTree {
 
         @Override
         public String toString() {
-            final StringBuilder sb = new StringBuilder("Node{");
+            final StringBuilder sb = new StringBuilder("Node1{");
             sb.append("value=").append(value);
             sb.append('}');
             return sb.toString();

2dc568c3942f9f4b4cb87170b7da49eb916bca28
Rafael Brito de Oliveira <peo_rboliveira@uolinc.com>
2016-12-15 18:31:43 -0200

    adding all the others algorithms tests

---
+++ src/BitManipulation.java
@@ -0,0 +1,9 @@
+/**
+ * Created by peo_rboliveira on 15/12/16.
+ */
+public class BitManipulation {
+
+    public static void main(String[] args) {
+        System.out.println(4 << 1 == 16);
+    }
+}
---
+++ src/FibonacciNumbers.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+/**
+ * Created by peo_rboliveira on 15/12/16.
+ */
+public class FibonacciNumbers {
+
+    public static int fibonacci(int n) {
+        if (n < 0) return 0;
+        if (n < 2) return n;
+        return fibonacci(n-1) + fibonacci(n - 2);
+    }
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        scanner.close();
+        System.out.println(fibonacci(n));
+    }
+}
